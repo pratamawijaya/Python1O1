8f76e5d9be4ca1214403d167173b2381452aa81a
Pratama <set.mnemonix@gmail.com>
2020-09-30 23:01:13 +0700

    move all to folder

---
+++ basic/sets.py
@@ -0,0 +1,14 @@
+# A set is a collection which is unordered and unindexed. In Python, sets are written with curly brackets.
+
+example_set = {"apple","banana","cherry"}
+
+print(example_set)
+
+# access items
+
+# print(example_set[0]) # cannot access sets via index
+
+for x in example_set:
+    print(x)
+
+print("banana" in example_set)
